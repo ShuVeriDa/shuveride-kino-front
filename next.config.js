98962cc8e76bc95da7740839cdbd38a7a750fb61
ShuVeriDa <bashtarov@outlook.com>
2022-11-08 15:47:55 +0300

    added settings Prettier and TSConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, //нужно выключить
   swcMinify: true,
+
+  poweredByHeader: false,
 }
 
 module.exports = nextConfig
