cd5660ef644a0ddff34b99243c2db8a209ffb505
ShuVeriDa <bashtarov@outlook.com>
2022-11-15 14:08:48 +0300

    added  Axios interceptors

---
+++ pages/manage/index.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+
+interface IAdminPageProps {
+}
+
+const AdminPage: NextPage<IAdminPageProps> = () => {
+  return (
+    <div>
+      Index
+    </div>
+  );
+};
+export default AdminPage
---
+++ pages/profile.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+
+interface IProfilePageProps {
+}
+
+const ProfilePage: NextPage<IProfilePageProps> = () => {
+  return (
+    <div>
+      Profile
+    </div>
+  );
+};
+export default ProfilePage
