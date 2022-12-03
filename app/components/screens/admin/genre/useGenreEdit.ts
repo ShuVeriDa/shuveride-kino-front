58d28a244afcc9284d8dc6c1240fc08f9022bec3
ShuVeriDa <bashtarov@outlook.com>
2022-12-03 17:11:57 +0300

    Added ability to edit genre

---
+++ app/components/screens/admin/genre/useGenreEdit.ts
@@ -0,0 +1,43 @@
+import {SubmitHandler, UseFormSetValue} from "react-hook-form";
+import {IGenreEditInput} from "@/screens/admin/genre/genre-edit.inteface";
+import {useRouter} from "next/router";
+import {useMutation, useQuery} from "react-query";
+import {GenreService} from "@/services/genre.service";
+import {toastError} from "@/utils/toastError";
+import {getKeys} from "@/utils/object/getKeys";
+import {toastr} from "react-redux-toastr";
+import {getAdminUrl} from "@/config/url.config";
+
+export const useGenreEdit = (setValue: UseFormSetValue<IGenreEditInput>) => {
+  const {push, query} = useRouter()
+
+  const genreId = String(query.id)
+
+  const {isLoading} = useQuery(['genre', genreId], () => GenreService.getById(genreId), {
+    onSuccess: ({data}) => {
+      getKeys(data).forEach(key => {
+        setValue(key, data[key])
+      })
+    },
+    onError: (error) => {
+      toastError(error, 'Get genre')
+    },
+    enabled: !!query.id
+  })
+
+  const {mutateAsync} = useMutation('update genre', (data:IGenreEditInput) => GenreService.update(genreId, data), {
+    onError: (error) => {
+      toastError(error, 'Update genre')
+    },
+    onSuccess: () => {
+      toastr.success("Update genre", 'update was successful')
+      push(getAdminUrl('genres'))
+    }
+  })
+
+  const onSubmit:SubmitHandler<IGenreEditInput> = async (data) => {
+    await mutateAsync(data)
+  }
+
+  return {onSubmit, isLoading}
+}
--- app/services/genre.service.ts
+++ app/services/genre.service.ts
@@ -2,6 +2,8 @@ import {getGenresUrl, getUsersUrl} from "@/config/api.config";
 import {IGenre} from "@/shared/types/movie.types";
 import {axiosClassic, instance} from "../api/interceptors";
 import {getGenreUrl} from "@/config/url.config";
+import {IGenreEditInput} from "@/screens/admin/genre/genre-edit.inteface";
+import axios from "axios";
 
 export const GenreService = {
   async getAll(searchTerm?: string) {
@@ -14,7 +16,16 @@ export const GenreService = {
     })
   },
 
-  async deleteGenre(_id: string) {
+  async getById(_id: string) {
+    return axios.get<IGenreEditInput>(getGenresUrl(`/${_id}`))
+  },
+
+
+  async update(_id: string, data: IGenreEditInput) {
+    return instance.put<string>(getGenreUrl(`/${_id}`), data)
+  },
+
+  async delete(_id: string) {
     return instance.delete<string>(getGenreUrl(`/${_id}`))
   }
 }
