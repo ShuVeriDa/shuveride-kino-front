08a3da48882000cc7dc3c15d7a971690a1c3266b
ShuVeriDa <bashtarov@outlook.com>
2023-05-02 14:34:48 +0300

    refactored

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -1,7 +1,7 @@
 import * as process from "process";
 
-export const API_URL = `${process.env.APP_URL}/api`
-export const API_SERVER_URL = `${process.env.APP_SERVER_URL}/api`
+export const API_URL = `${process.env.REACT_APP_API_URL}/api`
+export const API_SERVER_URL = `${process.env.REACT_APP_API_SERVER_URL}/api`
 
 export const getAuthUrl = (string: string) => `/auth${string}`
 export const getUsersUrl = (string: string) => `/users${string}`
